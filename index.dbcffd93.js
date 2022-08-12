bb62db17a99d01d0fa008bb9f5025daa41cb4be7
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2022-08-12 21:46:42 +0000

    Deploying to gh-pages from @ AnastasiyaSeroshtan/goit-js-hw-11@4e7417b44e66afd5f0e30d987d5406431f141024 🚀

---
+++ index.dbcffd93.js
@@ -0,0 +1,2 @@
+fetch("".concat("https://pixabay.com/api/","?key=").concat("29230862-8ed88c62e82238b6e063c75d0","&q=yellow+flowers&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1")).then((function(e){return e.json()})).then((function(e){return console.log(e)}));
+//# sourceMappingURL=index.dbcffd93.js.map
