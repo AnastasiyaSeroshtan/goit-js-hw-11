9fd4795cdc5ec0fa94bd6cb13ba0ec6bda8f0eb2
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2022-08-13 12:24:49 +0000

    Deploying to gh-pages from @ AnastasiyaSeroshtan/goit-js-hw-11@acac973a7762e1963de900c1dd2064586da617e7 🚀

--- index.16494390.js
+++
@@ -1,2 +0,0 @@
-fetch("https://pixabay.com/api/?key=29230862-8ed88c62e82238b6e063c75d0&q=yellow+flowers&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1").then((e=>e.json())).then((e=>console.log(e)));
-//# sourceMappingURL=index.16494390.js.map
